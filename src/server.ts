3b403903d0a8dd1559ebc6fd763fc21171d941b7
King-Lota <lottanna47@gmail.com>
2025-11-07 02:13:05 +0100

    configure to typescript

---
+++ src/server.ts
@@ -0,0 +1,26 @@
+import express, { Application, Request, Response } from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import helmet from "helmet";
+import { connectDB } from "./config/db";
+
+dotenv.config();
+
+const app: Application = express();
+const PORT = process.env.PORT || 5000;
+
+//Middleware
+app.use(express.json());
+app.use(cors());
+app.use(helmet());
+
+//connect to database
+connectDB();
+
+//test route
+app.get("/", (req: Request, res: Response) => {
+    res.send("Welcome to NAAPE API");
+});
+
+//listen to port
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
