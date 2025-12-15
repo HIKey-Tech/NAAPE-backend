3a8b2bf1757828ab215887b4395267e09b9ea823
King-Lota <lottanna47@gmail.com>
2025-12-15 17:27:16 +0100

    feat: add forgot password endpoint to authentication routes

--- src/routes/v1/auth.routes.ts
+++ src/routes/v1/auth.routes.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import { registerUser, loginUser } from "../../controllers/auth.controller";
+import { forgotPassword } from "../../controllers/reset.password.controller";
 
 const router = express.Router();
 
@@ -7,5 +8,8 @@ router.post("/register", registerUser);
 router.post("/login", loginUser);
 // router.post("/google", googleAuth);
 
+//forgot password
+
+router.post("/forgot-password", forgotPassword);
 
 export default router;
